71f5a043f9e58f2818ca9a4a3ab64d93011e2973
syril pagunsan <syril.pagunsan@godigitalcorp.com>
2022-03-24 15:04:47 +0800

    initial commit

---
+++ src/main/java/com/cybertactics/yahoo/hellojavaspring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.cybertactics.yahoo.hellojavaspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/api/hello/{name}")
+    @ResponseBody
+    public String getEmployeesById(@PathVariable String name) {
+        return "Hello " + name + " , how are you?";
+    }
+}
---
+++ src/test/java/com/cybertactics/yahoo/hellojavaspring/HelloJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertactics.yahoo.hellojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
